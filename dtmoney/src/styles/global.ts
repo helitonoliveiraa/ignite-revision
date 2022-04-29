808c88d19c43d565920a6e201b15fd484d92dea7
Héliton Oliveira <heliton.oliveira88@gmail.com>
2022-04-28 21:54:20 -0300

    :wrench: chore: setup global font

---
+++ dtmoney/src/styles/global.ts
@@ -0,0 +1,20 @@
+  '@font-face': [
+    { /* poppins-regular - latin */
+      fontFamily: 'Poppins',
+      fontStyle: 'normal',
+      fontWeight: 400,
+      src: "url('../fonts/poppins-v19-latin-regular.woff2')",
+    },
+    { /* poppins-500 - latin */
+      fontFamily: 'Poppins',
+      fontStyle: 'normal',
+      fontWeight: 500,
+      src: "url('../fonts/poppins-v19-latin-500.woff2')",
+    },
+    { /* poppins-600 - latin */
+      fontFamily: 'Poppins',
+      fontStyle: 'normal',
+      fontWeight: 600,
+      src: "url('../fonts/poppins-v19-latin-600.woff2')",
+    }
+  ], 

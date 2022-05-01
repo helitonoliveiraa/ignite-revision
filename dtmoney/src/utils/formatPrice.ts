2948323bd93bd3b9de504f0649a289ce2c0859a3
Héliton Oliveira <heliton.oliveira88@gmail.com>
2022-05-01 17:41:37 -0300

    :recycle: refactor: function to format price

--- dtmoney/src/utils/formatPrice.ts
+++ dtmoney/src/utils/formatPrice.ts
@@ -1,8 +1,17 @@
-export function formatPrice(amount: number) {
+type formatPriceProps = {
+  amount: number;
+  type?: 'income' | 'outcome';
+}
+
+export function formatPrice({ amount, type }: formatPriceProps) {
   const value = new Intl.NumberFormat('pt-BR', {
     style: 'currency',
     currency: 'BRL'
   }).format(amount);
+
+  if(type) {
+    return `${type === 'outcome' ? ' - ' : ''}${value}`;
+  }
 
   return value;
 }

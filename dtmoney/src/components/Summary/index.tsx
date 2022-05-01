274aca4dbb7e61eca5e79c6b5669a1bb78209c2c
Héliton Oliveira <heliton.oliveira88@gmail.com>
2022-05-01 17:44:20 -0300

    :sparkles: feat: culculate summary values

--- dtmoney/src/components/Summary/index.tsx
+++ dtmoney/src/components/Summary/index.tsx
@@ -1,16 +1,41 @@
 import { Card } from '../Card';
-import incomeImg from '../../assets/icons/income.svg';
-import outcomeImg from '../../assets/icons/outcome.svg';
-import totalImg from '../../assets/icons/total.svg';
+
+import { IncomeIcon, OutcomeIcon, TotalIcon } from '../../assets/icons';
 
 import * as S from './styles';
+import { useTransaction } from '../../hooks/useTransaction';
+import { formatPrice } from '../../utils';
 
 export function Summary() {
+  const { transactions } = useTransaction();
+
+  const { income, outcome, total } = transactions.reduce((accumulator, transaction) => {
+    if(transaction.type === 'income') {
+      accumulator.income += transaction.amount;
+      accumulator.total += transaction.amount;
+    } else {
+      accumulator.outcome += transaction.amount;
+      accumulator.total -= transaction.amount;
+    }
+
+    return  accumulator;
+  }, {
+    income: 0,
+    outcome: 0,
+    total: 0,
+  });
+
+  const summary = {
+    income: formatPrice({ amount: income }),
+    outcome: formatPrice({ amount: outcome }),
+    total: formatPrice({ amount: total }),
+  }
+
   return (
     <S.Container>
-      <Card title='Income' icon={incomeImg} value='R$ 17.400,00' />
-      <Card title='Outcome' icon={outcomeImg} value='R$ 17.400,00' />
-      <Card background='green' color='white' title='Total' icon={totalImg} value='R$ 17.400,00' />
+      <Card title='Income' icon={IncomeIcon} value={summary.income} />
+      <Card title='Outcome' icon={OutcomeIcon} value={summary.outcome} />
+      <Card background='green' color='white' title='Total' icon={TotalIcon} value={summary.total} />
     </S.Container>
   )
 }

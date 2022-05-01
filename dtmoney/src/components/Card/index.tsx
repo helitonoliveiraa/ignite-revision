e4905619f1d88a5270abe7b0d18bb9eb82336140
Héliton Oliveira <heliton.oliveira88@gmail.com>
2022-05-01 12:00:50 -0300

    :wrench: refactor: to use icon component

--- dtmoney/src/components/Card/index.tsx
+++ dtmoney/src/components/Card/index.tsx
@@ -1,24 +1,23 @@
-import { ComponentProps } from 'react';
+import { ComponentProps, ElementType } from 'react';
 
 import { Paragraph } from '../Paragraph';
 import { Strong } from '../Strong';
-import { Image } from '../Image';
 
 import * as S from './styles';
 
 type CardProps = ComponentProps<typeof S['Container']> & {
   title: 'Income' | 'Outcome' | 'Total';
   value: string;
-  icon: string;
+  icon: ElementType;
   color?: 'default' | 'white';
 }
 
-export function Card({ title = 'Income', value, icon, background = 'default', color = 'default', ...props }: CardProps) {
+export function Card({ title = 'Income', value, icon: Icon, background = 'default', color = 'default', ...props }: CardProps) {
   return (
     <S.Container {...props} background={background}>
       <S.Header>
         <Paragraph color={color}>{title}</Paragraph>
-        <Image src={icon} alt={title} size='default' />
+        <Icon />
       </S.Header>
 
       <Strong color={color}>{value}</Strong>
